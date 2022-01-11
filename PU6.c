e4f149238b55e1eed753ad437e0fd7863ac77e8c
JurisNoviks <90375429+JurisNoviks@users.noreply.github.com>
2022-01-11 10:45:07 +0200

    Update PU6.c

--- PU6.c
+++ PU6.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
+#include <windows.h>
 
-void noargnoreturn()
+// Bez arg un bez return itkaaa
+void pirmais()
 {
-    printf("Funkcija bez argumentiem un return vertibam\n");
+    printf("Vnk teksts, bez funkcijas un argumentiem \n");
 }
 
-void argnoreturn (int ag1, char ag2, char * ag3)
-{
-    printf("Funkcija ar argumentiem bez return\n");
-}
-
-int noargreturn ()
-{
-    printf("Funkcija bez argumentiem bet ar return\n");
-    return 1;
-}
-
-int argreturn (int ag1, char ag2, char * ag3)
-{
-    printf("Funkcija ar argumentiem un return vrtb\n");
-    return 1;
-}
-
+// ar agr un bez return
 int main()
 {
-    noargnoreturn();
-    argnoreturn(1, 2, NULL);
-    noargreturn();
-    argreturn(1, 2, NULL);
+    int x, y, z;
+    cin >> x >> y;
+    z = x+y;
+}
+
+// Bez argumentiem, bet ar return
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+// Ar argumentu un return
+int
+main (void)
+{
+  MessageBox (0, "Hello, World!",
+              "Message", MB_OK);
+  return 0;
+}
+
+// ar agr un ar return
+int main()
+{
+    int x, y, z;
+    cin >> x >> y;
+    z = x+y;
+    count << z<< endl;
+    return 0;
 }
