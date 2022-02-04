a968bef78dd2a68f471dc4f9716a727ef7f0cd30
JurisNoviks <90375429+JurisNoviks@users.noreply.github.com>
2022-02-04 12:24:47 +0200

    Update pu4_while_factorial.c

--- pu4_while_factorial.c
+++ pu4_while_factorial.c
@@ -3,34 +3,60 @@
 int main()
 {
     char tips;
-    char test;
+    
    
     
-    int int_skaitlis,int_rezultats,i;
-    int_skaitlis=int_rezultats=i = 1;
+    int int_skaitlis;
+    int_skaitlis = 1;
     
-    long long l_skaitlis,l_rezultats,l_i;
-    l_skaitlis=l_rezultats=l_i = 1;
+    long long l_skaitlis;
+  
     
+  
+    
+    switch(select()) {
+     case 'c':
+      
+        charFactorial();
+        
+     break;
+     
+     case 'i':
+     scanf("%d",&int_skaitlis);
+     intFaktorial(int_skaitlis);
+       
+     break;
+     
+     case 'l':
+        scanf("%lld",&l_skaitlis);
+        
+       
+        printf("Faktorials no %lld ir : %lld",l_skaitlis,longFactorial(l_skaitlis));
+     break;
+     
+     
+     
+    default:
+    printf("Nepareizs datu tips \n");
+    }
+    return 0;
+}
+void charFactorial()
+{
+    char test;
     char char_skaitlis,c_i,char_rezultats;
     char_skaitlis=c_i=char_rezultats = 1;
-   
-    printf("Ievadiet datu tipu c=char,i=integer, l=long long \n");
-    scanf(" %c",&tips);
-    printf("Ievadiet skaitli \n");
-    switch(tips) {
-     case 'c':
-        scanf("%d",&char_skaitlis);
-        
-        char_rezultats = c_i = 1;//abi char ir nules kautvai ieprieks pieskrta vērtiba 1.Tapėc pieskiru vertibu velreiz
-        
-        for(c_i;c_i<=char_skaitlis;c_i++)
+    scanf("%d",&char_skaitlis);
+
+ char_rezultats = c_i = 1;
+    while(c_i<=char_skaitlis)
         {
             test = (char_rezultats*c_i); //ja tiek ielikts pa taisno ifa tad nestrada (char sasniendz lielaku vertibu neka var)
             if(test >= char_rezultats)
             {
                 
                 char_rezultats*=c_i;
+                c_i++;
                 
             }
             else
@@ -42,16 +68,17 @@ int main()
             
         }
         printf("Faktorials no %d ir : %d",char_skaitlis,char_rezultats);
-     break;
-     
-     case 'i':
-     scanf("%d",&int_skaitlis);
-        for(i = 1;i<=int_skaitlis;i++)
+}
+void intFaktorial(int int_skaitlis)
+{
+    int int_rezultats,i;
+    int_rezultats=i = 1;
+     while(i<=int_skaitlis)
         {
             if((int_rezultats*i) >= int_rezultats)
             {
                 int_rezultats*=i;
-            
+            i++;
             }
             else
             {
@@ -62,15 +89,25 @@ int main()
             
         }
         printf("Faktorials no %d ir : %d",int_skaitlis,int_rezultats);
-     break;
-     
-     case 'l':
-        scanf("%lld",&l_skaitlis);
-        for(l_i;l_i<=l_skaitlis;l_i++)
+}
+int select()
+{
+    char tips;
+    printf("Ievadiet datu tipu c=char,i=integer, l=long long \n");
+    scanf(" %c",&tips);
+    printf("Ievadiet skaitli \n");
+    return tips;
+}
+int longFactorial(long long l_skaitlis)
+{
+    long long l_rezultats = 1;
+    long long l_i = 1;
+    while(l_i<=l_skaitlis)
         {
             if((l_rezultats*l_i) >= l_rezultats)
             {
                 l_rezultats*=l_i;
+            l_i++;
             }
             else
             {
@@ -80,13 +117,5 @@ int main()
            
             
         }
-        printf("Faktorials no %lld ir : %lld",l_skaitlis,l_rezultats);
-     break;
-     
-     
-     
-    default:
-    printf("Nepareizs datu tips \n");
-    }
-    return 0;
+    return l_rezultats;
 }
